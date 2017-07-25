0e962316b2bdc88c150f4e2ff14599cf4d8ded59
josdem <joseluis.delacruz@gmail.com>
2017-07-24 20:02:52 -0500

    Adding call to retrofit

--- app/src/main/java/com/jos/dem/retrofit/MainActivity.java
+++ app/src/main/java/com/jos/dem/retrofit/MainActivity.java
@@ -2,6 +2,14 @@ package com.jos.dem.retrofit;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.jos.dem.retrofit.model.Category;
+import com.jos.dem.retrofit.service.JugoterapiaService;
+
+import java.util.List;
+
+import retrofit2.Call;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,6 +17,11 @@ public class MainActivity extends AppCompatActivity {
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+    JugoterapiaService jugoterapiaService = JugoterapiaService.retrofit.create(JugoterapiaService.class);
+    List<Category> call = jugoterapiaService.getCategories();
+    for(Category category: call){
+      Log.d("Category", category.getName());
+    }
   }
 
 }
