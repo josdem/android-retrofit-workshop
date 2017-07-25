5464d7e6d2fdcfb1311bf7dd85f28d12e5a2a378
josdem <joseluis.delacruz@gmail.com>
2017-07-24 19:42:20 -0500

    Adding category model

--- app/src/main/java/com/jos/dem/retrofit/MainActivity.java
+++ app/src/main/java/com/jos/dem/retrofit/MainActivity.java
@@ -1,4 +1,4 @@
-package dem.jos.com.retrofit;
+package com.jos.dem.retrofit;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -10,4 +10,5 @@ public class MainActivity extends AppCompatActivity {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
   }
+
 }
