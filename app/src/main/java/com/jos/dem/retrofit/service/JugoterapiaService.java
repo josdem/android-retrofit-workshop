599b783cb68222213061f2a292e4e02925adb202
josdem <joseluis.delacruz@gmail.com>
2017-07-24 19:49:14 -0500

    Adding service

---
+++ app/src/main/java/com/jos/dem/retrofit/service/JugoterapiaService.java
@@ -0,0 +1,21 @@
+package com.jos.dem.retrofit.service;
+
+import com.jos.dem.retrofit.model.Category;
+
+import java.util.List;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+import retrofit2.http.GET;
+
+public interface JugoterapiaService {
+
+  @GET("/jugoterapia-server/beverage/categories")
+  public List<Category> getCategories();
+
+  public static final Retrofit retrofit = new Retrofit.Builder()
+          .baseUrl("http://jugoterapia.josdem.io/")
+          .addConverterFactory(GsonConverterFactory.create())
+          .build();
+
+}
