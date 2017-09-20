3fd9c38ba4197bf69e45d8e882b3f008bd743aeb
josdem <joseluis.delacruz@gmail.com>
2017-09-20 15:38:50 -0500

    Adding credentials model

--- app/src/main/java/com/jos/dem/retrofit/service/JugoterapiaService.java
+++ app/src/main/java/com/jos/dem/retrofit/service/JugoterapiaService.java
@@ -1,18 +1,26 @@
 package com.jos.dem.retrofit.service;
 
 import com.jos.dem.retrofit.model.Category;
+import com.jos.dem.retrofit.model.Credentials;
 
 import java.util.List;
 
 import retrofit2.Call;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
+import retrofit2.http.Body;
 import retrofit2.http.GET;
+import retrofit2.http.Headers;
+import retrofit2.http.POST;
 
 public interface JugoterapiaService {
 
   @GET("/jugoterapia-server/beverage/categories")
   public Call<List<Category>> getCategories();
+
+  @Headers("Content-Type: application/json")
+  @POST("http://192.168.43.233:8080/auth/validate")
+  public Call<Credentials> sendCredentials(@Body Credentials credentials);
 
   public static final Retrofit retrofit = new Retrofit.Builder()
           .baseUrl("http://jugoterapia.josdem.io/")
