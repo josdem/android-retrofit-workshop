b072a597745790f3961f8d594d555398dc59e823
josdem <joseluis.delacruz@gmail.com>
2024-08-09 18:09:00 -0400

    Setting Retrofit version as value

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,6 +49,8 @@ android {
     }
 }
 
+val retrofitVersion = "2.11.0"
+
 dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
@@ -58,8 +60,8 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
-    implementation("com.squareup.retrofit2:retrofit:2.11.0")
-    implementation("com.squareup.retrofit2:converter-gson:2.11.0")
+    implementation("com.squareup.retrofit2:retrofit:$retrofitVersion")
+    implementation("com.squareup.retrofit2:converter-gson:$retrofitVersion")
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
